adfdc9571859d8f4bad0ca0802a53f653330e255
Vicente Quiles Sáez <vicenteqsdam@gmail.com>
2018-03-21 19:51:25 +0100

    Creación de TEST

--- src/test/java/es/vqs/pruebaVideoJenkins/PruebasTest.java
+++ src/test/java/es/vqs/pruebaVideoJenkins/PruebasTest.java
@@ -1,5 +1,12 @@
 package es.vqs.pruebaVideoJenkins;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PruebasTest {
 
+	@Test
+	public void dummyTest() {
+		Assert.assertTrue(true);
+	}
 }
